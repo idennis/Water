8b692af48f830919fc8eea8d1224d0e3c05de1c3
Dennis Hou <dennis@fillr.com>
2017-08-28 16:14:02 +1000

    Added new view controller

---
+++ Water/WaterViewController.h
@@ -0,0 +1,13 @@
+//
+//  WaterViewController.h
+//  Water
+//
+//  Created by fillr on 28/8/17.
+//  Copyright © 2017 dennishou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WaterViewController : NSViewController
+
+@end
---
+++ Water/WaterViewController.m
@@ -0,0 +1,26 @@
+//
+//  WaterViewController.m
+//  Water
+//
+//  Created by dennishou on 28/8/17.
+//  Copyright © 2017 dennishou. All rights reserved.
+//
+
+#import "WaterViewController.h"
+
+@interface WaterViewController ()
+
+@end
+
+@implementation WaterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+
+
+@end
